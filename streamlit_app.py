6851d5d3bf61264301a3b06181de0eac93fe5fcc
bhumi <bhumid2025@gmail.com>
2025-06-04 20:50:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ model = joblib.load("bestmodel.pkl")  # or just "best_model.pkl" if it's in root
 
 st.title("Satellite Predictive Maintenance")
 
-EXPECTED_FEATURES = 10  # Replace with your model's input feature count
+EXPECTED_FEATURES = 6  # Replace with your model's input feature count
 
 st.write(f"Enter {EXPECTED_FEATURES} features:")
 inputs = [st.number_input(f"Feature {i+1}") for i in range(EXPECTED_FEATURES)]
